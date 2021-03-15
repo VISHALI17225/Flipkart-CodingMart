fef1abe70e5d06e4623f8793daf01b74e56a9945
Vishali <45118364+VISHALI17225@users.noreply.github.com>
2021-03-15 20:51:23 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import SearchField from "react-search-field";
+
+function App() {
+  return (
+     
+    <div className="App" >
+      <header className="App-header">
+        <img src="/logo512.png" className="App-logo" alt="logo" />
+        
+        <SearchField
+  placeholder="Search for Products, bands and more"
+    classNames="test-class"
+  />      
+        
+        <a
+          className="App-link"
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Login
+        </a>
+        </header>
+        <main>
+          <p> Main Content</p>
+        </main>
+       
+      
+    </div>
+    
+  );
+}
+
+export default App;
