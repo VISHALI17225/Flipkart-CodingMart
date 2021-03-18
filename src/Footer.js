0d9b9df87ac046b61dc635ad45500ec5691a92d6
Vishali <45118364+VISHALI17225@users.noreply.github.com>
2021-03-18 13:47:00 +0530

    Add files via upload

---
+++ src/Footer.js
@@ -0,0 +1,156 @@
+import React from 'react';
+import './Footer.css';
+
+
+function Footer()
+{
+    return(
+    
+     
+        <div className="iui">
+        <div className="container">
+
+         <div className="row text-center text-xs-center text-sm-left text-md-left">
+             <br></br>
+             <br></br>
+         </div>
+
+        
+            <div className="row text-center text-xs-center text-sm-left text-md-left">
+                <div className="col-xs-12 col-sm-2 col-md-2">
+                    
+                    <ul className="list-unstyled quick-links">
+                        <li>ABOUT</li>
+                        <li><a  className="foot-com" href="https://www.fiverr.com/share/qb8D02"><i ></i>Contact Us</a></li>
+                        <li><a  className="foot-com" href="https://www.fiverr.com/share/qb8D02"><i ></i>About Us</a></li>
+                        <li><a  className="foot-com" href="https://www.fiverr.com/share/qb8D02"><i ></i>Careers</a></li>
+                        <li><a className="foot-com" href="https://www.fiverr.com/share/qb8D02"><i ></i>Flipkart Stories</a></li>
+                        <li><a className="foot-com" href="https://www.fiverr.com/share/qb8D02"><i ></i>Press</a></li>
+                    
+                        <li><a className="foot-com" href="https://www.fiverr.com/share/qb8D02"><i ></i>Flipkart Wholesale</a></li>
+                        
+
+                    </ul>
+                </div>
+                <div className="col-xs-12 col-sm-2 col-md-2">
+                    
+                    <ul className="list-unstyled quick-links">
+                        <li>HELP</li>
+                        <li><a  className="foot-com" href="https://www.fiverr.com/share/qb8D02"><i ></i>Payments</a></li>
+                        <li><a  className="foot-com" href="https://www.fiverr.com/share/qb8D02"><i ></i>Shipping</a></li>
+                        <li><a  className="foot-com" href="https://www.fiverr.com/share/qb8D02"><i ></i>Cancellation & Returns</a></li>
+                        <li><a className="foot-com" href="https://www.fiverr.com/share/qb8D02"><i ></i>FAQ</a></li>
+                        <li><a className="foot-com" href="https://www.fiverr.com/share/qb8D02"><i ></i>Report Infringement</a></li>
+                    
+                       
+
+                    </ul>
+                </div>
+
+                <div className="col-xs-12 col-sm-2 col-md-2">
+                    
+                    <ul className="list-unstyled quick-links">
+                        <li>POLICY</li>
+                        <li><a  className="foot-com" href="https://www.fiverr.com/share/qb8D02"><i ></i>Return Policy</a></li>
+                        <li><a  className="foot-com" href="https://www.fiverr.com/share/qb8D02"><i ></i>Terms Of Use</a></li>
+                        <li><a  className="foot-com" href="https://www.fiverr.com/share/qb8D02"><i ></i>Security</a></li>
+                        <li><a className="foot-com" href="https://www.fiverr.com/share/qb8D02"><i ></i>Privacy</a></li>
+                        <li><a className="foot-com" href="https://www.fiverr.com/share/qb8D02"><i ></i>Sitemap</a></li>
+                    
+                        <li><a className="foot-com" href="https://www.fiverr.com/share/qb8D02"><i ></i>EPR Compliance</a></li>
+                        
+
+                    </ul>
+                </div>
+                <div className="col-xs-12 col-sm-2 col-md-2">
+                    
+                    <ul className="list-unstyled quick-links">
+                        <li>SOCIAL</li>
+                        <li><a  className="foot-com" href="https://www.fiverr.com/share/qb8D02"><i ></i>Facebook</a></li>
+                        <li><a  className="foot-com" href="https://www.fiverr.com/share/qb8D02"><i ></i>Twitter</a></li>
+                        <li><a  className="foot-com" href="https://www.fiverr.com/share/qb8D02"><i ></i>YouTube</a></li>
+                        
+
+                    </ul>
+                </div>
+                <div className="col-xs-12 col-sm-2 col-md-2">
+                    
+                    <ul className="list-unstyled quick-links" style={{fontSize:"12px"}}>
+                        <li>Mail Us:</li>
+                        <br></br>
+                        <li>Flipkart Internet Private Limited</li>
+                        <li>Building Alyssa,Begonia &</li>
+                        <li>Clove Embassy Tech Village,</li>
+                        <li>Outer Ring Road,Devarabeesanahalli Village,</li>
+                        <li>Bengaluru,560103</li>
+                    
+                        <li>Karnataka,India</li>
+                        
+    
+
+                    </ul>
+                </div>
+
+
+                <div className="col-xs-12 col-sm-2 col-md-2" >
+                    
+                    <ul className="list-unstyled quick-links" style={{fontSize:"12px"}}>
+                        <li style={{fontSize:"12px"}}>Registered Office Address</li>
+                        <br></br>
+                        
+                        <li>Flipkart Internet Private Limited</li>
+                        <li>Building Alyssa,Begonia &</li>
+                        <li>Clove Embassy Tech Village,</li>
+                        <li>Outer Ring Road,Devarabeesanahalli Village,</li>
+                        <li>Bengaluru,560103</li>
+                    
+                        <li>Karnataka,India</li>
+                        <li>CIN:U51109KA2012PTC066107</li>
+                        <li style={{color:"blue"}}>Telephone: 1800 202 9898</li>
+
+
+                    </ul>
+                </div>
+
+
+             
+            </div>
+            <hr color="white" />  
+
+            <div className="row text-center text-xs-center text-sm-left text-md-left">
+                <div className="col-xs-12 col-sm-2 col-md-2">
+                    <ul className="list-unstyled quick-links">
+                    <li><a  className="foot-head" href="https://www.fiverr.com/share/qb8D02"><i ></i>Payment Methods</a></li>
+
+                    </ul>
+                </div>
+            </div>
+          
+    
+            <div className="row text-center text-xs-center text-sm-left text-md-left">      
+        
+            <img style={{width:"80px"}} className="ic" src="https://cdn0.iconfinder.com/data/icons/payment-vol-3/100/NETBanking_credit_debit_card_bank_transaction-128.png" /> 
+            <img style={{width:"80px"}} className="ic" src="https://cdn0.iconfinder.com/data/icons/payment-vol-3/100/Visa_credit_debit_card_bank_transaction-128.png" /> <i></i>
+            <img style={{width:"80px"}} className="ic" src="https://cdn1.iconfinder.com/data/icons/picons-social/57/social_mastercard-128.png" />
+           
+            </div>
+            <br />
+            <br />
+            <br />
+
+
+        
+
+
+
+                
+                
+
+                
+            </div>
+        </div>
+
+       
+    )
+}
+export default Footer;
